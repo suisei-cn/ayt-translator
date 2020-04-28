a33e89cdc74cb681ea5dc2d3bfa95312b2760036
Gary Guo <gary@garyguo.net>
2020-04-28 18:26:05 +0100

    Emoji recovery in userscript

--- userscript.js
+++ userscript.js
@@ -18,10 +18,28 @@
 
   let acceptableTexts = ['Translate Tweet', '翻译推文'];
 
+  function extractText(element) {
+    if (element.nodeName === '#text') {
+      return element.textContent;
+    }
+    if (element.nodeName === 'IMG') {
+      let match = /emoji\/v2\/svg\/([0-9a-f]+)\.svg$/.exec(element.src);
+      if (match) {
+        return String.fromCodePoint(parseInt(match[1], 16));
+      }
+      return '';
+    }
+    let builder = '';
+    for (let child of element.childNodes) {
+      builder += extractText(child);
+    }
+    return builder;
+  }
+
   document.body.addEventListener('click', event => {
     if (acceptableTexts.indexOf(event.target.textContent) !== -1) {
       translateSpan = event.target;
-      let text = event.target.parentNode.previousSibling.textContent;
+      let text = extractText(event.target.parentNode.previousSibling);
       translateSpan.textContent = 'Translating...';
       GM_xmlhttpRequest({
         method: "POST",
@@ -34,7 +52,6 @@
           let response = JSON.parse(resp.responseText);
           if (response.translation) {
             translatedText = document.createElement('div');
-            translatedText
             translatedText.textContent = response.translation;
             translatedText.className = translateSpan.parentNode.previousSibling.className;
             translateSpan.textContent = 'Translated by suisei-cn Auto Translator';
