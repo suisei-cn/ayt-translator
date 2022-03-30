ecf8e33ffe28edec2e9cb9d01418b3a1e25254a1
Gary Guo <gary@garyguo.net>
2022-03-30 17:18:55 +0100

    Keep tabs always rendered

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
   const { t } = useTranslation();
   return (
     <Pivot style={{ height: '100%' }} styles={{ itemContainer: { height: 'calc(100% - 44px)' } }}>
-      <PivotItem style={{ height: '100%' }} headerText={t("tab-editor")}>
+      <PivotItem alwaysRender style={{ height: '100%' }} headerText={t("tab-editor")}>
         <TermManager />
       </PivotItem>
-      <PivotItem style={{ height: '100%' }} headerText={t("tab-test")}>
+      <PivotItem alwaysRender style={{ height: '100%' }} headerText={t("tab-test")}>
         <Tester />
       </PivotItem>
     </Pivot>
